918b242fc92b08435ae4abb634cf343905a4627f
Estefania <estefaniaromero.torrents@gmail.com>
2022-12-19 20:52:22 +0100

    feat:boto +1

--- src/exercicis/Comptador/components/display/Display.js
+++ src/exercicis/Comptador/components/display/Display.js
@@ -1,9 +1,22 @@
 import "./Display.css";
+import { useState } from "react";
 
 function Display() {
+  const [count, setCount] = useState(0);
   return (
     <>
-      <Display />
+      <div className="div_Input">
+        <input type="text" id="fname" name="fname" />
+      </div>
+      <div className="div_Input">
+        <button onClick={() => setCount((valorActual) => valorActual + 1)}>
+          + 1
+        </button>
+        <button>- 1</button>
+      </div>
+      <div className="div_Input">
+        <button>+ 5</button>
+      </div>
     </>
   );
 }
